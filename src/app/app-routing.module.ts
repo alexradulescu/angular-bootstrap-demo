c4647800af8de4c4008837f27922164d7709f755
Alex Radulescu <radulescu.alexandru.doru@gmail.com>
2019-03-24 15:46:01 +0800

    Budget update 2
    - Added overview
    - Added expense, income and categories list
    - Added category create form dialog
    - UI changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ExpenseListComponent } from './expense-list/expense-list.component';
+import { IncomeListComponent } from './income-list/income-list.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+
+const routes: Routes = [
+    { path:'', pathMatch:'full', redirectTo:'home' },
+    { path:'home', component:HomeComponent },
+
+    { path:'expense-list', component:ExpenseListComponent },
+    { path:'expenses', pathMatch:'full', redirectTo:'expense-list' },
+
+
+    { path:'income-list', component:IncomeListComponent },
+    { path:'incomes', pathMatch:'full', redirectTo:'expense-list' },
+
+
+    { path:'settings', component:CategoryListComponent },
+    
+    { path:'**', pathMatch:'full', redirectTo:'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
